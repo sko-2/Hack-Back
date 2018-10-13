3b220304f439d85ce8dde03a8669899737f39814
Kyle Glandt <kyle.glandt@gmail.com>
2018-10-12 22:59:51 -0500

    Added a test object to be serialized to json

--- src/main/java/unk/HackBack/controllers/Hello.java
+++ src/main/java/unk/HackBack/controllers/Hello.java
@@ -2,12 +2,13 @@ package unk.HackBack.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import unk.HackBack.Models.TestObject;
 
 @RestController
 public class Hello {
 
     @GetMapping("api/hello")
-    public String helloWorld() {
-        return "WAZZZUP BITCHES";
+    public TestObject helloWorld() {
+        return new TestObject("wazzup", "bitches");
     }
 }
