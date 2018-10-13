e1e5daf3e6eab4739eded1112d0b55ec75edf5c3
Kyle Glandt <kyle.glandt@gmail.com>
2018-10-12 23:32:59 -0500

    Added CORS

--- src/main/java/unk/HackBack/controllers/Hello.java
+++ src/main/java/unk/HackBack/controllers/Hello.java
@@ -1,9 +1,11 @@
 package unk.HackBack.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import unk.HackBack.Models.TestObject;
 
+@CrossOrigin
 @RestController
 public class Hello {
 
