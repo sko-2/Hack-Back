7c87b12cd0619d10867dc4a07a0892ed3bc01d59
Kyle Glandt <kyle.glandt@gmail.com>
2018-10-12 22:32:11 -0500

    Initial commit

---
+++ src/main/java/unk/HackBack/HackBackApplication.java
@@ -0,0 +1,12 @@
+package unk.HackBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackBackApplication.class, args);
+	}
+}
---
+++ src/test/java/unk/HackBack/HackBackApplicationTests.java
@@ -0,0 +1,16 @@
+package unk.HackBack;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HackBackApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
